9d2aec05ded3083d3467e90fec52b39661616a8b
AshwinBengaluru <brashwinkumar10@gmail.com>
2022-05-11 12:47:10 +0530

    Change to CRUDRepo to solve getUserApi issue

--- src/main/java/com/namepro/pass/repository/UserCredRepository.java
+++ src/main/java/com/namepro/pass/repository/UserCredRepository.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.namepro.pass.model.UserCred;
+import org.springframework.data.repository.CrudRepository;
 
 
-public interface UserCredRepository extends JpaRepository<UserCred, Long> {
+public interface UserCredRepository extends CrudRepository<UserCred, Long> {
   List<UserCred> findByUsernameContaining(String username);
+
+  List<UserCred> findByUsername(String username);
 }
